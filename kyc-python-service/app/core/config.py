3af792d03b2968c7867f4a6a54feb8be110f14d3
pondkarun <52126943+pondkarun@users.noreply.github.com>
2025-04-16 10:30:35 +0700

    conda

--- kyc-python-service/app/core/config.py
+++ kyc-python-service/app/core/config.py
@@ -1,7 +1,11 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    base_image_url: str
+    host: str = "localhost"
+    port: int = 8000
+    reload: bool = False
+    database_url: str
+    base_image_url: str = "http://localhost:3000"
 
     class Config:
         env_file = ".env"
